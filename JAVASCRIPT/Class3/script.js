7ff666b849a6bce78f879a0d6228333c1f04c825
SurajKumar429 <surajkumarnegi45@gmail.com>
2025-08-18 18:39:42 +0530

    Loops in JS

---
+++ JAVASCRIPT/Class3/script.js
@@ -0,0 +1,70 @@
+//! Loops in JS
+//? for loop
+//for(initializatio; condition; itteration/updation){
+    //instruction
+//}
+
+// for (let i = 0; i < 10; i++){
+//     console.log(i);
+// }
+
+// let marks = [90, 98, 70, 10, 21, 34, 56, 76, 80]
+// for (let i = 0; i < marks.length; i++){
+//     console.log(marks[i]);
+// }
+
+//? While Loop
+// initialization
+// while(condition){
+//     //instructions
+//     updation
+// }
+
+//? Do While
+// initialization
+// do{
+//    //instruction
+//    //updation 
+// }while(condition)
+
+//!for-of
+// let str = "Hello"
+// for (let s of str){
+//     console.log(s)
+// }
+// let marks = [90, 98, 70, 10, 21, 34, 56, 76, 80]
+// for (let m of marks){
+//     console.log(m)
+// }
+
+//? For In
+// let obj = {
+//     name: "SURAJ",
+//     age: 22,
+//     mark: 98,
+//     address: "BBSR"
+// }
+
+// for (let key in obj){
+//     console.log(key);
+//     console.log(obj[key]);
+// }
+
+//? Array Inbuilt Methods
+let arr = [1, 2, 3, 4, 65, 8, 9]
+console.log(arr);
+//! push()
+// arr.push(10, 11, true)
+// console.log(arr);
+
+//?pop()
+// arr.pop()
+// console.log(arr);
+
+//? Unshift()
+// arr.unshift("Suraj", undefined, null)
+// console.log(arr);
+
+//?Shift
+// arr.shift();
+// console.log(arr);
