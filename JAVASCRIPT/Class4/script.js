c06f91affbf156c9cf48c326144dc64ec9bb5441
SurajKumar429 <surajkumarnegi45@gmail.com>
2025-08-20 18:16:31 +0530

    Functions in JS

---
+++ JAVASCRIPT/Class4/script.js
@@ -0,0 +1,142 @@
+//! Conditional Statements
+//? if statement
+// let age = -30
+// if (age >= 18){
+//     console.log("You are an adult");
+// }
+
+//? if-else
+    // if(age >= 18){
+    //     console.log("You are a major");
+    // }
+    // else{
+    //     console.log("You are a minor");
+    // }
+
+    //! else-if
+    // if(age>=18){
+    //     console.log("You are an adult");
+    // }else if(age > 0 && age < 18){
+    //     console.log("You are a minor");
+    // }else{
+    //     console.log("Invalid Age");
+    // }
+
+    //? Switch case
+    // let val = 2
+    // switch(val){
+    //     case 1:
+    //         console.log("Case One");
+    //         break
+    //         case 2:
+    //             console.log("Case Two");
+    //             break
+    //             case 3:
+    //                 console.log("case 3");
+    //                 break
+    //                 case 4:
+    //                     console.log("Case 4");
+    //                     break
+    //                     default:
+    //                         console.log("Default case");
+    // }
+
+
+    //? Array Inbuilt Methods
+    // let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    // console.log(arr);
+
+    // //! splice()
+    // arr.splice(2,0, "Hi", "Hello", true)//add
+    // console.log(arr);
+    //  arr.splice(1,2, "Hi", "Hello", true)//replace element
+    //  console.log(arr);
+    //   arr.splice(3,3)//delete element
+    //   console.log(arr);
+
+
+    //? slice()
+    // let subArr = arr.slice(2,8)
+    // console.log(subArr);
+
+    //? includes()
+    // console.log(arr.includes(2));
+
+    //?reverse()
+    // arr.reverse()
+    // console.log(arr);
+
+    //? toString()
+    // let strArr = arr.toString()
+    // console.log(strArr);
+
+
+    //? String and it's Inbuilt methods
+    let name = "Suraj"
+    let age = 22
+    // console.log("My name is", name, "and my age is", age);
+
+    //? Template string
+    // console.log(`Hello Everyone`);
+
+    //?Template Literals
+    // console.log(`My name is ${name} and my age is ${age}`);
+
+    //?Inbuilt Methods
+    let str = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum vel praesentium quaerat sint aliquid suscipit eum officia illo id eaque rem ipsa culpa natus esse facilis, beatae repudiandae error expedita?"
+    // str[1] = 'a'
+    // console.log(str);
+
+    //? toUpperCase()
+    // console.log(str.toUpperCase());
+
+    //? toLowerCase
+    // console.log(str.toLowerCase());
+
+    //? indexof()
+    // console.log(str.indexOf("ipsum"));
+
+    //?lastIndexof()
+    // console.log(str.lastIndexOf("ipsum"));
+
+    //? charAt()
+    // console.log(str.charAt(10));
+
+    //?slice()
+    // console.log(str.slice(1,10));
+
+    //?replace()
+    // let substr = str.replace("ipsum","a");
+    // console.log(substr);
+    // console.log(str);
+
+    //? replaceAll()
+    //  let replastr = str.replaceAll("ipsum", "b")
+    //  console.log(replastr);
+
+    //? trim()
+    // console.log(str.trim());
+
+    //? length
+    // console.log(str.length);
+
+    //? split()
+    // let arrStr = str.split(" ")
+    // console.log(arrStr);
+    
+    
+    
+     
+    
+    
+    
+    
+    
+    
+    
+
+    
+    
+    
+
+
---
+++ JAVASCRIPT/Class5/script.js
@@ -0,0 +1,118 @@
+//! User-Defined Function
+//? Normal Function
+// funtion functionName(){
+//     //instructions
+// }
+
+function printSomething(){
+    console.log("This is a normal function");
+}
+
+// printSomething()
+
+// *Without Parameter
+function sum(){
+    return 10 + 20
+}
+let a = sum()
+// console.log(a);
+
+//* With Paramater
+function add(a, b){
+    return a + b
+}
+
+let x = add(20,20)
+// console.log(x);
+let y = add(100, 200)
+// console.log(y);
+
+// console.log(add(1));
+
+//* FUnction with Default Vlaue
+function sub(x = 0, y = 0){
+    console.log("x value is", x);
+    console.log("y value is", y);
+
+    return(x-y)
+}
+// console.log(sub());
+
+//? Arrow Function
+// const functionName = ()=>{
+//     //instruction
+// }
+
+const sayHello = ()=>{
+    console.log("Hello Everyone");    
+}
+// sayHello()
+
+//? Anonymous Function
+// const Varible = function(){
+//     //instructions
+// }
+
+const multiple = function(){
+    console.log(10 * 2);
+}
+// multiple()
+
+//? Callback Function
+// ()=>{
+
+// }
+
+//* forEach loop
+// Array.forEach(callbackFunction)
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// arr.forEach((x)=> {
+//     console.log(x)
+// }
+// )
+
+// let arr1 = [8, 9, true, undefined]
+// arr1.forEach((x,i)=> {
+//     console.log("Value of X:", x);
+//     console.log("Value of I:", i);
+    
+// }
+// )
+
+//! map()
+let marks = [50, 55, 45, 56, 76, 89]
+// let newMarks = marks.forEach((mark) => {
+//     return mark + 5
+// })
+// console.log(newMarks);
+
+// let updatedMarks = marks.map((mark,i)=>{
+//     console.log(mark);
+//     console.log(i);
+//     return mark + 5
+// })
+// console.log(updatedMarks);
+
+// let nmarks = marks.map((mark)=>{
+//     if(mark >= 55){
+//         console.log(mark);
+//         return mark
+//     }
+// })
+// console.log(nmarks);
+
+//! filter()
+// const filterMarks = marks.filter((mark)=>{
+//     return mark >= 55
+// })
+// console.log(filterMarks);
+
+//! reduce()
+let total = marks.reduce((sum,mark)=>sum + mark,0)
+console.log(total);
+
+
+
+
+
